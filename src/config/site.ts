c7c590bc3cb2a50fb28944d51c420b9cf671e883
XXXXXuan <chenxuanyi88@hotmail.com>
2026-09-23 21:29:39 +0100

    Initial academic homepage

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+export const site = {
+  name: "Xuanyi Chen",
+
+  title:"Xuanyi Chen | Academic Homepage",
+
+  description:
+    "Postdoctoral Researcher at John Innes Centre in Myriam Charpentier's Lab",
+
+  url: "https://xxxxxuan.github.io",
+
+  author: "Xuanyi Chen",
+
+  locale: "en-GB",
+
+  image: "/og-image.jpg",
+};
